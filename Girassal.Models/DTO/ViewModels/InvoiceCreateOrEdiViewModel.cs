d4f618f7bcda912fb722c4b40083c87b8a435bff
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-03-18 20:04:40 +0200

    InvoiceCreateOrEdiViewModel

---
+++ Girassal.Models/DTO/ViewModels/InvoiceCreateOrEdiViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Girassol.Models.DTO.ViewModels
+{
+    public class InvoiceCreateOrEdiViewModel : Invoice
+    {
+    }
+}
